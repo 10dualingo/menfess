a85ea6c803848ae131da836cf410c7b426526ae6
Jason <development@pnxl.dev>
2024-09-17 08:38:13 +0700

    styles: style template

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="app"
-    class="min-h-screen min-w-full bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200"
+    class="min-h-screen min-w-full justify-center flex bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200"
   >
     <router-view v-slot="{ Component }">
       <transition name="fade" mode="out-in">
